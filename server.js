bdca362dd84a4d9023d7d276538cf9ebed263b36
zharrow <florent.detres@protonmail.com>
2025-03-08 20:34:23 +0100

    Adding Docker

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const PORT = process.env.PORT || 3000
 prisma.$connect().then(async () => {
     console.log('Connected to the database')
 
-    app.listen(PORT, () => {
+    app.listen(PORT, '0.0.0.0', () => {
         console.log('Server is running on port 3000');
     })
 
